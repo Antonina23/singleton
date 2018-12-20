8053c08f596aa2cb6074538af37416eea1420c8d
Antonina23 <tone4kawow@gmail.com>
2018-12-20 22:01:24 +0300

    Update class Logger

--- app.rb
+++ app.rb
@@ -1,7 +1,25 @@
 class Logger
+  def initialize
+    @f = File.open 'log.txt', 'a'
+  end
+
+  def self.instance
+    puts "OK"
+  end
+
+# cless method
   def self.say_something
     puts "Hello!"
+  end
+
+# instance method
+  def log_something what
+    @f.puts what
   end
 end
 
 Logger.say_something
+Logger.instance
+
+logger = Logger.new
+logger.log_something 'Wazzzzup'
