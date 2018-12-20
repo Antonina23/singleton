9efe1f28f8838681ec58ddac555961e074eb8bd5
Antonina23 <tone4kawow@gmail.com>
2018-12-20 22:21:08 +0300

    Refactoring logger

--- app.rb
+++ app.rb
@@ -1,31 +1,4 @@
-class Logger
-  def initialize
-    @f = File.open 'log.txt', 'a'
-  end
+require './logger'
 
-  @@x = nil
-
-  def self.instance
-    if @@x == nil
-      @@x = Logger.new
-    end
-    return @@x
-  end
-
-# cless method
-  def self.say_something
-    puts "Hello!"
-  end
-
-# instance method
-  def log_something what
-    @f.puts what
-  end
-end
-
-Logger.say_something
 Logger.instance.log_something 'Hello there'
-Logger.instance.log_something 'Hello everyone'
 
-logger = Logger.new
-logger.log_something 'Wazzzzup'
